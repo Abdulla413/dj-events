fb40f7b6d486420b088c974716b63ab1ab3d2409
abdulla <abdulla.arish@protonmail.com>
2022-04-08 12:19:40 +0200

    changed

---
+++ dj-events-frontend/pages/events/index.js
@@ -0,0 +1,9 @@
+import Layout from "../../components/Layout";
+
+import React from 'react'
+
+export default function EventsPage() {
+  return (
+    <Layout title="Event Page" >EventsPage</Layout >
+  )
+}
